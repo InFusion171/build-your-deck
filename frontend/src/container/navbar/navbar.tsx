67c42bd6c810afcd28ef1dc129cafbe04337057e
Marius Degenhardt <marius.degenhardt@stud.uni-goettingen.de>
2024-07-25 12:18:24 +0200

    added navbar

--- frontend/src/container/navbar/navbar.tsx
+++ frontend/src/container/navbar/navbar.tsx
@@ -6,6 +6,16 @@ const Navbar = () => {
   
   return (
     <>
+      <div className='navbar-container'>
+        <div className='navbar-logo'>
+            <p>Logo</p>
+        </div>
+        <div className='navbar-account'>
+            <p>Accounts</p>
+        </div>
+      </div>
+      
+      <hr />
     </>
   )
 }
