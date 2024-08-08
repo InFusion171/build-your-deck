69415438bfaf6010c51424603169b0df72b83e94
Marius Degenhardt <marius.degenhardt@stud.uni-goettingen.de>
2024-08-08 14:20:35 +0200

    added exec query and renamed files

--- backend/Database.py
+++ backend/Database.py
@@ -7,12 +7,18 @@ class Database:
         self.database_name = database_name
 
         self.db_conn = sqlite3.connect(database_name)
+        self.cursor = self.db_conn.cursor()
 
     def createTopPlayersDatabase(self, top_players: dict):
         player_list = [{'playerTag' : tag, 'elo': elo} for elo, tag in top_players.items()]
 
         df = pd.DataFrame(player_list)
         df.to_sql(self.table_name, self.db_conn, if_exists='replace', index=False)
+
+    def exec_query(self, query: str):
+        self.cursor.execute(query)
+
+        return self.cursor.fetchall()
 
     def __del__(self):
         self.db_conn.close()
