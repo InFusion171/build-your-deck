02dd35265f71973c6edf95e5619e8cd0d16a857b
Marius Degenhardt <marius.degenhardt@stud.uni-goettingen.de>
2024-08-13 13:29:39 +0200

    added create_connection method

--- backend/Database.py
+++ backend/Database.py
@@ -7,6 +7,15 @@ class Database:
     def __init__(self, database_path: str, table_name: str):
         self.table_name = table_name
         self.database_path = database_path
+        self.connection = None
+        self.cursor = None
+
+    def create_connection(self):
+        if not self.database_exists():
+            print(f'{self.database_path} dont exist. Will create a new DB')
+
+        self.connection = sqlite3.connect(self.database_path)
+        self.cursor = self.connection.cursor()
 
     def __enter__(self):
         if not self.database_exists():
@@ -22,6 +31,10 @@ class Database:
 
 
     def exec_query(self, query: str) -> list[Any]:
+        if self.cursor is None:
+            print('Need to open the Database connection before executing the query')
+            return None
+
         self.cursor.execute(query)
 
         return self.cursor.fetchall()
